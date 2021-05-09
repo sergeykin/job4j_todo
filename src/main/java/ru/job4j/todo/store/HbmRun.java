cf299be78c9dd4a9f18a872630b44b22bab08d10
sergeykin <sergeykinn@mail.ru>
2021-05-09 21:16:31 +0300

    2. ManyToMany [#456747]

--- src/main/java/ru/job4j/todo/store/HbmRun.java
+++ src/main/java/ru/job4j/todo/store/HbmRun.java
@@ -6,10 +6,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import ru.job4j.todo.model.CarBrand;
-import ru.job4j.todo.model.CarModel;
-import ru.job4j.todo.model.Role;
-import ru.job4j.todo.model.User;
+import ru.job4j.todo.model.*;
+
 import java.util.List;
 
 public class HbmRun {
@@ -21,13 +19,40 @@ public class HbmRun {
             Session session = sf.openSession();
             session.beginTransaction();
 
-            CarModel carModel1 = CarModel.of("carModel1");
-            CarModel carModel2 = CarModel.of("carModel2");
+            Book book1 = Book.of("book1");
+            Book book2 = Book.of("book2");
 
-            CarBrand carBrand1 = CarBrand.of("carBrand");
-            carBrand1.getCarModels().add(carModel1);
-            carBrand1.getCarModels().add(carModel2);
-            session.save(carBrand1);
+            Author author1 = Author.of("author1");
+            Author author2 = Author.of("author2");
+
+            author1.getBooks().add(book1);
+            author1.getBooks().add(book2);
+
+            author2.getBooks().add(book1);
+            session.persist(author1);
+            session.persist(author2);
+//            Person person = session.get(Person.class, 1);
+//            session.remove(person);
+//            Address one = Address.of("Kazanskaya", "1");
+//            Address two = Address.of("Piterskaya", "10");
+//
+//            Person first = Person.of("Nikolay");
+//            first.getAddresses().add(one);
+//            first.getAddresses().add(two);
+//
+//            Person second = Person.of("Anatoliy");
+//            second.getAddresses().add(two);
+//
+//            session.save(first);
+//            session.save(second);
+
+//            CarModel carModel1 = CarModel.of("carModel1");
+//            CarModel carModel2 = CarModel.of("carModel2");
+//
+//            CarBrand carBrand1 = CarBrand.of("carBrand");
+//            carBrand1.getCarModels().add(carModel1);
+//            carBrand1.getCarModels().add(carModel2);
+//            session.save(carBrand1);
 //            User one = User.of("Petr");
 //            User two = User.of("Andrei");
 //
