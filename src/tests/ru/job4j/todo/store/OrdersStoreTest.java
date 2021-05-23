b69aef11e7d8a36d99beec709b3a844ed546a7b7
sergeykin <sergeykinn@mail.ru>
2021-05-23 14:21:47 +0300

    0. Что такое интеграционное тестирование. [#6875]

---
+++ src/tests/ru/job4j/todo/store/OrdersStoreTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.todo.store;
+
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.job4j.todo.model.Order;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class OrdersStoreTest {
+    private BasicDataSource pool = new BasicDataSource();
+
+    @Before
+    public void setUp() throws SQLException {
+        pool.setDriverClassName("org.hsqldb.jdbcDriver");
+        pool.setUrl("jdbc:hsqldb:mem:tests;sql.syntax_pgs=true");
+        pool.setUsername("sa");
+        pool.setPassword("");
+        pool.setMaxTotal(4);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream("./db/update_001.sql")))
+        ) {
+            br.lines().forEach(line -> builder.append(line).append(System.lineSeparator()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pool.getConnection().prepareStatement(builder.toString()).executeUpdate();
+    }
+
+    @Test
+    public void whenSaveOrderAndFindAllOneRowWithDescription() throws SQLException {
+        OrdersStore store = new OrdersStore(pool);
+
+        store.save(Order.of("name1", "description1"));
+
+        List<Order> all = (List<Order>) store.findAll();
+
+        assertThat(all.size(), is(1));
+        assertThat(all.get(0).getDescription(), is("description1"));
+        assertThat(all.get(0).getId(), is(1));
+    }
+
+    @Test
+    public void whenUpdate() throws SQLException {
+        OrdersStore store = new OrdersStore(pool);
+
+        Order order = store.save(Order.of("name1", "description1"));
+        order.setDescription("description2");
+        store.update(order);
+        List<Order> all = (List<Order>) store.findAll();
+
+        assertThat(all.size(), is(1));
+        assertThat(all.get(0).getDescription(), is("description2"));
+        assertThat(all.get(0).getId(), is(1));
+    }
+
+    @Test
+    public void whengetName() throws SQLException {
+        OrdersStore store = new OrdersStore(pool);
+
+        Order order = store.save(Order.of("name1", "description1"));
+        Order order1 = store.findByName(order.getName());
+
+
+        assertThat(order.getDescription(), is(order1.getDescription()));
+
+    }
+}
